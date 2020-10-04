bc3e7e5670a680366318f2ff7e185f47bb56e225
Varis Darasirikul <varis.dara@gmail.com>
2020-10-04 15:24:39 +0800

    feature/create_bootcamp_endpoint-add-mongoose: Update module and structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { BootcampsModule } from './controllers/bootcamps/bootcamps.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BootcampsModule } from './modules/bootcamps.module';
 
 const mongoUrl =
   process.env.NODE_ENV === 'production'
