a68eb5f89ad76be4edc09acacf5e2fedaa2a640b
manjarb <manjarb4@gmail.com>
2020-10-06 18:37:44 +0800

    Finish course model and seeding

--- src/modules/bootcamps.module.ts
+++ src/modules/bootcamps.module.ts
@@ -11,6 +11,6 @@ import { MongoProviderModule } from './mongo-provider.module';
   imports: [MongoProviderModule, ConfigModule],
   controllers: [BootcampsController],
   providers: [BootcampsService, AdvancedQueryService, GeocoderService],
-  exports: [MongoProviderModule, BootcampsService],
+  exports: [MongoProviderModule],
 })
 export class BootcampsModule {}
